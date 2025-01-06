fefcfa9135416aa51f5b45da00e6a2ec3f80b17b
tushar-3549 <tushardiu3549@gmail.com>
2025-01-06 16:56:04 +0600

    update front-end and implement py files p1

---
+++ app/crud.py
@@ -0,0 +1,20 @@
+from sqlalchemy.orm import Session
+import app.models as models
+
+def create_translation_task(db: Session, text: str, languages: list):
+    task = models.TranslationTask(text=text, languages=languages)
+    db.add(task)
+    db.commit()
+    db.refresh(task)
+    return task
+
+def get_translation_task(db: Session, task_id: int):
+    return db.query(models.TranslationTask).filter(models.TranslationTask.id == task_id).first()
+
+def update_translation_task(db: Session, task_id: int, translations: dict):
+    task = db.query(models.TranslationTask).filter(models.TranslationTask.id == task_id).first()
+    task.translations = translations
+    task.status = "completed"
+    db.commit()
+    db.refresh(task)
+    return task
--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@ from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from app import schemas
+from app import crud
 
 app = FastAPI()
 templetes = Jinja2Templates(directory="app/templates")
@@ -12,4 +14,15 @@ def home():
 @app.get('/index', response_class=HTMLResponse)
 def index(request: Request):
     return templetes.TemplateResponse("index.html", {"request": request})
-    
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"],
+)
+
+@app.post('/translate', response_model=schemas.TaskResponse)
+def translate(request: schemas.TranslationRequest):
+    pass # next start from here
